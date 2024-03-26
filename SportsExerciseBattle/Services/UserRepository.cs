74f3333fee071f29070f67aa481ebd9c18fe94c6
CaptainKnobel <cklein@live.at>
2024-03-26 14:18:38 +0100

    Added "using System.ComponentModel.DataAnnotations;" to all Models and prepared a Data Access Layer "UserRepository.cs" class in folder "Services"

--- SportsExerciseBattle/Services/UserRepository.cs
+++ SportsExerciseBattle/Services/UserRepository.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
+using SEB.Models;
 
-namespace SportsExerciseBattle.Services
+namespace SEB.Services
 {
-    internal class UserRepository
+    public class UserRepository
     {
+        private readonly string connectionString;
+
+        public UserRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void AddUser(User user)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                // TODO: Perform SQL insert operation to add user to database
+            }
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                // TODO: Perform SQL select operation to retrieve user by username from database
+                // TODO: Construct and return a User object
+                return null; // TODO: Placeholder, to be replaced with actual implementation
+            }
+        }
+
+        // TODO: what ever other methods I need
     }
 }
