96df66695b9bc69750de897f2310db799c34b066
CaptainKnobel <cklein@live.at>
2024-03-26 15:47:24 +0100

    Added PushUpRecordService.cs for the Services Layer

---
+++ SportsExerciseBattle/Services/PushUpRecordService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SEB.Models;
+using SEB.Services;
+
+namespace SEB.Services
+{
+    public class PushUpRecordService
+    {
+        private readonly PushUpRecordRepository pushUpRecordRepository;
+
+        public PushUpRecordService(PushUpRecordRepository pushUpRecordRepository)
+        {
+            this.pushUpRecordRepository = pushUpRecordRepository;
+        }
+
+        public void AddPushUpRecord(PushUpRecord record)
+        {
+            // TODO: Perform additional validation or business logic as needed
+            pushUpRecordRepository.AddPushUpRecord(record);
+        }
+
+        // what ever other methods for retrieving push-up records, calculating statistics, etc.
+    }
+}
+
