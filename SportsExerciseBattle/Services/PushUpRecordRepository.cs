0e780462cfd9f5eb565cabd964c6c3527f5ef2ee
CaptainKnobel <cklein@live.at>
2024-03-26 14:29:38 +0100

    Added more Data Access Layer classes in Services named "...Repository". Actual Database must be implemented later on.

---
+++ SportsExerciseBattle/Services/PushUpRecordRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using SEB.Models;
+
+namespace SEB.Services
+{
+    public class PushUpRecordRepository
+    {
+        private readonly string connectionString;
+
+        public PushUpRecordRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void AddPushUpRecord(PushUpRecord record)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                // TODO: Perform SQL insert operation to add push-up record to database
+            }
+        }
+
+        // TODO: what ever other methods I need
+    }
+}
---
+++ SportsExerciseBattle/Services/TournamentRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using SEB.Models;
+
+namespace SEB.Services
+{
+    public class TournamentRepository
+    {
+        private readonly string connectionString;
+
+        public TournamentRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void StartTournament(Tournament tournament)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                // TODO: Perform SQL insert operation to start a new tournament
+            }
+        }
+
+        // TODO: what ever other methods I need
+    }
+}
