31761561d73e791f9a26f7db621346d09bdaed81
CaptainKnobel <cklein@live.at>
2024-04-19 07:59:38 +0200

    asfasgfaws gwag ag

--- SportsExerciseBattle.Tests/UnitTest1.cs
+++ SportsExerciseBattle.Tests/UnitTest1.cs
@@ -1,16 +1,26 @@
+using NUnit.Framework;
+using SportsExerciseBattle.Web.HTTP;  // Assuming your classes to test are in this namespace
+
 namespace SportsExerciseBattle.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class HttpRequestTests
     {
+        private HttpRequest request;
+
         [SetUp]
         public void Setup()
         {
+            // Setup code to initialize HttpRequest object before each test
+            var reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes("GET / HTTP/1.1\r\n\r\n")));
+            request = new HttpRequest(reader);
         }
 
         [Test]
-        public void Test1()
+        public void TestHttpRequestMethodIsGet()
         {
-            Assert.Pass();
+            request.Parse();
+            Assert.AreEqual(HttpMethod.GET, request.Method, "The HTTP method should be GET.");
         }
     }
 }
